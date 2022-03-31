a4ce77b4b231aa006478aef7cf643a61b6d4d842
Maheep <maheeppartap47@gmail.com>
2022-03-30 20:11:58 -0700

    finished IDA*

--- Heuristic/__init__.py
+++ Heuristic/__init__.py
@@ -1,4 +1,9 @@
+import Board
+
 
 class Heuristic:
     def __init__(self):
+        pass
+
+    def score(self, board: Board.board):
         pass
--- Search/IDA_star.py
+++ Search/IDA_star.py
@@ -1,10 +1,30 @@
 import Search
 import Board
+import operator
 
 
-class IDA_star (Search.Search):
-    def __init__(self):
-        pass
+class IDA_star(Search.Search):
+    def __init__(self, node_expansion, heuristic, depth: int):
+        super().__init__(node_expansion, heuristic)
+        self.depth = depth
+        self.heuristic = heuristic
 
-    def decision(self, board: Board.Board ):
-        pass
+    def _decision(self, board: Board.Board, depth: int):
+        if depth == 0:
+            return self.heuristic.score(board)
+        possible_moves_list = list(board.legal_moves())
+
+        moves_score_combo = {}
+
+        # Expand nodes - room for improvement given too much redundancy
+        for move in possible_moves_list:
+            board._make_move_in_board(move)
+            moves_score_combo[move] = self._decision(board, depth - 1)
+            board.board.pop()
+
+        max_key = max(moves_score_combo, key=moves_score_combo.get)
+
+        return moves_score_combo[max_key]
+
+    def decision(self, board: Board.Board):
+        best_move = self._decision(board, self.depth)
